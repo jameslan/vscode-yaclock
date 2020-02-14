13f91411370a8a881f78cccb9ab84568efd1b6af
James Lan <jameslan@gmail.com>
2020-02-13 23:41:27 -0800

    :recycle: Refactor a Clock class

---
+++ src/clock.ts
@@ -0,0 +1,73 @@
+import * as vscode from 'vscode';
+
+export class Clock {
+    clockItem: vscode.StatusBarItem;
+    schedule: number;
+    // configs
+    expireInSeconds: boolean;
+    prefix: string;
+    postfix: string;
+    hour12: boolean;
+    showAmPm: boolean;
+    showDate: boolean;
+    showDay: boolean;
+    showSecond: boolean;
+    flash: boolean;
+    showSeparator: boolean;
+    constructor() {
+        const config = vscode.workspace.getConfiguration('yaclock');
+        this.prefix = config.get('prefix', '');
+        this.postfix = config.get('postfix', '');
+        this.hour12 = config.get('hour12', false);
+        this.showAmPm = this.hour12 && config.get('showAmPm', false);
+        this.showDate = config.get('showDate', false);
+        this.showDay = config.get('showDay', false);
+        this.showSecond = config.get('showSecond', false);
+        this.flash = config.get('flashTimeSeparator', false);
+        this.showSeparator = true;
+        this.expireInSeconds = this.showSecond || this.flash || false;
+
+        this.clockItem = vscode.window.createStatusBarItem(
+            config.get('position', 'right') === 'right'
+                ? vscode.StatusBarAlignment.Right
+                : vscode.StatusBarAlignment.Left,
+            config.get('priority', 10),
+        );
+        this.clockItem.show();
+        this.update = this.update.bind(this);
+        this.schedule = setTimeout(this.update, 0);
+    }
+    dispose() {
+        this.clockItem.dispose();
+        clearTimeout(this.schedule);
+    }
+    renderText(time: Date) {
+        let options: Intl.DateTimeFormatOptions = {};
+        if (this.showDate) {
+            options = { day: 'numeric', month: 'short' };
+        }
+        if (this.showDay) {
+            options.weekday = 'short';
+        }
+        const day = (this.showDate || this.showDay) ? time.toLocaleDateString(undefined, options) + ' ' : '';
+        const separator = this.showSeparator ? ':' : ' ';
+        if (this.flash) {
+            this.showSeparator = !this.showSeparator;
+        }
+        const hour = this.hour12 ? (time.getHours() + 11) % 12 + 1 : time.getHours();
+        const minute = time.getMinutes().toString().padStart(2, '0');
+        const second = this.showSecond ? separator + time.getSeconds().toString().padStart(2, '0') : '';
+        const ampm = this.showAmPm ? (time.getHours() < 12 ? ' AM' : ' PM') : '';
+        this.clockItem.text = `${this.prefix}${day}${hour}${separator}${minute}${second}${ampm}${this.postfix}`;
+    }
+    renderTooltip(time: Date) {
+        this.clockItem.tooltip = time.toLocaleString();
+    }
+    update() {
+        const time = new Date();
+        const interval = this.expireInSeconds ? 1000 - time.getMilliseconds() : 60000 - time.getMilliseconds() - time.getSeconds() * 1000;
+        this.schedule = setTimeout(this.update, interval);
+        this.renderText(time);
+        this.renderTooltip(time);
+    }
+}
--- src/extension.ts
+++ src/extension.ts
@@ -1,102 +1,20 @@
 import * as vscode from 'vscode';
-
-interface Renderer {
-    renderText(time: Date): string;
-    renderTooltip(time: Date): string;
-    expireInSeconds: boolean;
-}
-
-// returns the render, as well as a flag indicating if it needs to be called on every second
-function rendererGen(): Renderer {
-    const config = vscode.workspace.getConfiguration('yaclock');
-    const prefix = config.get<string>('prefix');
-    const postfix = config.get<string>('postfix');
-    const hour12 = config.get<boolean>('hour12');
-    const showAmPm = hour12 && config.get<boolean>('showAmPm');
-    const showDate = config.get<boolean>('showDate');
-    const showDay = config.get<boolean>('showDay');
-    const showSecond = config.get<boolean>('showSecond');
-    const flash = config.get<boolean>('flashTimeSeparator');
-    let showSeparator = true;
-
-    return {
-        renderText(time: Date) {
-            let options: Intl.DateTimeFormatOptions = {};
-            if (showDate) {
-                options = { day: 'numeric', month: 'short' };
-            }
-            if (showDay) {
-                options.weekday = 'short';
-            }
-            const day = (showDate || showDay) ? time.toLocaleDateString(undefined, options) + ' ' : '';
-
-            const separator = showSeparator ? ':' : ' ';
-            if (flash) {
-                showSeparator = !showSeparator;
-            }
-
-            const hour = hour12 ? (time.getHours() + 11) % 12 + 1 : time.getHours();
-            const minute = time.getMinutes().toString().padStart(2, '0');
-
-            const second = showSecond ? separator + time.getSeconds().toString().padStart(2, '0') : '';
-
-            const ampm = showAmPm ? (time.getHours() < 12 ? ' AM' : ' PM') : '';
-
-            return `${prefix}${day}${hour}${separator}${minute}${second}${ampm}${postfix}`
-        },
-
-        renderTooltip(time: Date) {
-            return time.toLocaleString();
-        },
-
-        expireInSeconds: showSecond || flash || false,
-    };
-}
-
-function createClock(): vscode.StatusBarItem {
-    const config = vscode.workspace.getConfiguration('yaclock');
-    return vscode.window.createStatusBarItem(
-        config.get<string>('position') === 'left' ? vscode.StatusBarAlignment.Left : vscode.StatusBarAlignment.Right,
-        config.get<number>('priority'));
-}
+import { Clock } from './clock';
 
 export function activate(context: vscode.ExtensionContext) {
-    let clockItem: vscode.StatusBarItem;
-    let renderer: Renderer;
-    let schedule: number;
+    let clock: Clock;
 
     let subscriptions = context.subscriptions
 
-    const update = () => {
-        const time = new Date();
-        const interval = renderer.expireInSeconds ? 1000 - time.getMilliseconds() : 60000 - time.getMilliseconds() - time.getSeconds() * 1000;
-        schedule = setTimeout(update, interval);
-
-        clockItem.text = renderer.renderText(time);
-        clockItem.tooltip = renderer.renderTooltip(time);
-    }
-
-    const setup = () => {
-        clockItem = createClock();
-        renderer = rendererGen();
-        subscriptions.push(clockItem);
-        clockItem.show();
-    };
-
-    const cleanup = () => {
-        clockItem.dispose();
-        clearTimeout(schedule);
-    }
-
-    setup();
-    update();
+    clock = new Clock();
+    subscriptions.push(clock);
 
     vscode.workspace.onDidChangeConfiguration(e => {
         if (!e.affectsConfiguration('yaclock')) {
             return;
         }
-        cleanup();
-        setup();
-        update();
+        clock.dispose();
+        clock = new Clock();
+        subscriptions.push(clock);
     });
 }
